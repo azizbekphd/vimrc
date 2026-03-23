50c3d66296555e44ba664a7423ee1df16f5187f5
Azizbek <azizbekphd@gmail.com>
2026-03-23 13:13:45 +0500

    fix autocomplete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -80,17 +80,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+require('luasnip.loaders.from_vscode').lazy_load()
+
 cmp.setup({
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
